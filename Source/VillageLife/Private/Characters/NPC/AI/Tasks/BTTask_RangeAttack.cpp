933a24aedbdb3bbb2ed107aad970ef8be6fed60d
Luis Santos <lasantos2@outlook.com>
2025-02-03 17:31:22 -0700

    Further range attack task modification.

--- Source/VillageLife/Private/Characters/NPC/AI/Tasks/BTTask_RangeAttack.cpp
+++ Source/VillageLife/Private/Characters/NPC/AI/Tasks/BTTask_RangeAttack.cpp
@@ -2,4 +2,42 @@
 
 
 #include "Characters/NPC/AI/Tasks/BTTask_RangeAttack.h"
+#include "AIController.h"
+#include "GameFramework/Character.h"
+#include "BehaviorTree/BlackBoardComponent.h"
 
+EBTNodeResult::Type UBTTask_RangeAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* MyController{ OwnerComp.GetAIOwner() };
+	if (ensure(MyController))
+	{
+		ACharacter* MyPawn{ Cast<ACharacter>(MyController->GetPawn()) };
+		if (MyPawn == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		// replace socket location string with something better
+		// Doesn't exist for us, just following the tutorial for now.
+		FVector HornLocation { MyPawn->GetMesh()->GetSocketLocation("HornMagic_Socket") };
+
+		AActor* TargetActor{ Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("TargetActor")) };
+		if (TargetActor == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		FVector Direction{ TargetActor->GetActorLocation() - HornLocation };
+		FRotator HornRotation{ Direction.Rotation() };
+
+
+		FActorSpawnParameters Params;
+		Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		AActor* NewProjectile{ GetWorld()->SpawnActor<AActor>(ProjectileClass, HornLocation, HornRotation, Params) };
+
+		return NewProjectile ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+	}
+
+	return EBTNodeResult::Failed;
+}
--- Source/VillageLife/Public/Characters/NPC/AI/Tasks/BTTask_RangeAttack.h
+++ Source/VillageLife/Public/Characters/NPC/AI/Tasks/BTTask_RangeAttack.h
@@ -13,5 +13,11 @@ UCLASS()
 class VILLAGELIFE_API UBTTask_RangeAttack : public UBTTaskNode
 {
 	GENERATED_BODY()
-	
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	TSubclassOf<AActor> ProjectileClass;
 };
