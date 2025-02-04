9970e6245f23ff800a8d67686930a8f077694459
Luis Santos <lasantos2@outlook.com>
2025-02-03 17:31:01 -0700

    Created task for Villager

---
+++ Source/VillageLife/Public/Characters/NPC/AI/Tasks/BTTask_RangeAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_RangeAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VILLAGELIFE_API UBTTask_RangeAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
